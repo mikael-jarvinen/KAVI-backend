6e06f6ca76b90137c5168e7b27ce7cc8c4522c0c
usermine12 <mikaeljarv@gmail.com>
2020-05-05 14:41:08 +0300

    restore route for handling React route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
 const express = require('express')  //Express is used for static fileserving
 const app = express()
+const path = require('path')
 const cors = require('cors')
 
 const PORT = process.env.PORT
@@ -17,6 +18,15 @@ server.applyMiddleware({ app })
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'), err => {
+    if (err) {
+      res.status(500).send(err)
+      console.log(err)
+    }
+  })
+})
 
 app.listen({ port: PORT}, () => {
   console.log(`Apollo server ready at http://localhost:${PORT}${server.graphqlPath}`)
